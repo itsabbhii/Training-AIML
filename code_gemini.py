31d3dc39ba78f9dfbb711134c4d0d77c80832c10
Abhinav <0221csds100@gmail.com>
2024-11-15 00:26:25 +0530

    gitignore

--- code_gemini.py
+++ code_gemini.py
@@ -1,7 +1,9 @@
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
