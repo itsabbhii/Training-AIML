ca7707c79065833d30f9c71ca51320ab5792af2f
Abhinav <0221csds100@gmail.com>
2024-11-15 00:36:45 +0530

    updated

--- code_openai.py
+++ code_openai.py
@@ -1,5 +1,7 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
