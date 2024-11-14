71c79749a0aef0c3d31a31a683ab1c84202913f0
Abhinav <0221csds100@gmail.com>
2024-11-15 00:36:45 +0530

    loop

--- code_gemini.py
+++ code_gemini.py
@@ -6,7 +6,9 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 model = genai.GenerativeModel("gemini-1.5-flash")
-
-request = input("Enter your request")
-response = model.generate_content(request)
-print(response.text)
+while True:
+    request = input("Enter your request: ")
+    if request=="exit":
+        break
+    response = model.generate_content(request)
+    print(response.text)
